0661cf861b983f157a0ae9b5f08b0a846a46db7a
Eslem <j.eslem03@gmail.com>
2019-08-16 08:47:51 +0200

    Basic animation

---
+++ App.js
@@ -0,0 +1,92 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  View,
+  Image,
+  Button,
+  LayoutAnimation,
+  UIManager,
+} from 'react-native';
+
+import rocket from './assets/rocket.png';
+
+UIManager.setLayoutAnimationEnabledExperimental &&
+  UIManager.setLayoutAnimationEnabledExperimental(true);
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {position: 'flex-start'};
+  }
+
+  changePosition = position => {
+    LayoutAnimation.easeInEaseOut();
+    this.setState({
+      position,
+    });
+  };
+
+  render() {
+    return (
+      <SafeAreaView style={{flex: 1}}>
+        <View style={[styles.container, {justifyContent: this.state.position}]}>
+          <Image style={styles.logo} source={rocket} />
+          <View style={styles.buttonsContainer}>
+            <Button
+              style={styles.button}
+              title="Top"
+              onPress={() => this.changePosition('flex-start')}
+            />
+            <Button
+              style={styles.button}
+              title="Middle"
+              onPress={() => this.changePosition('center')}
+            />
+            <Button
+              style={styles.button}
+              title="Bottom"
+              onPress={() => this.changePosition('flex-end')}
+            />
+          </View>
+        </View>
+      </SafeAreaView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+    paddingTop: 64,
+    paddingBottom: 32,
+  },
+
+  logo: {
+    width: 200,
+    resizeMode: 'contain',
+  },
+  buttonsContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    position: 'absolute',
+    bottom: 16,
+    width: '100%',
+  },
+  button: {
+    width: 100,
+  },
+});
+
+export default App;
